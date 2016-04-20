2e7b16c4e1a0a7bbfc114d08e1f25256f48980f0
Jounter <ruben.n147@gmail.com>
2016-04-20 15:14:03 +0100

    asd

--- resources/views/adicionarExploracao.blade.php
+++ resources/views/adicionarExploracao.blade.php
@@ -1,90 +1,98 @@
-@extends('app')
-
-@section('customStyles')
-<link href="../../css/addExploracao.css" rel="stylesheet">
-
-@endsection
-@section('content')
-<div class="container">
-	<div class="row centered-form">
-		<div class="col-xs-12 col-sm-9 col-md-8  col-sm-offset-3 col-md-offset-3">
-			<div>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h3 class="modal-title" id="myModalLabel">Exploração Agrícola</h3>
-					</div>
-					<div class="panel-body">
-						<form id="registerForm" method="POST" action="/exploracoes/adicionar/submit" >
-						<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">		
-							<div class="form-group">
-								<fieldset> 
-									<legend>Dados do Terreno</legend>
-									<div class="col-xs-12 col-md-12">
-										<label for="nome">Nome do Terreno</label>
-										<div class="input-group">
-											<input type="text" class="form-control" id="nome"  name="nome" placeholder="Insira o nome do terreno" required><span class="input-group-addon"><i class="glyphicon glyphicon-asterisk"></i></span>
-										</span>										
-									</div>
-									@if( Session::get('message'))
-									<div style="text-align: center">
-										<span class="alert alert-info"> {{ Session::get('message') }}</span>
-									</div>
-									@endif
-									<br>
-									<label for="numero">Número do Terreno</label>
-									<div>
-										<input type="number" class="form-control" id="numero"  name="numero" min=0 placeholder="Insira o número de registo do terreno">
-									</span>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Adicionar Exploração</title>
+	<!-- Bootstrap core CSS -->
+	<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="{{asset('css/geral.css')}}" rel="stylesheet">
+	<link href="{{asset('../../css/addExploracao.css')}}" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="row centered-form">
+			<div class="col-xs-12 col-sm-9 col-md-8  col-sm-offset-3 col-md-offset-3">
+				<div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="modal-title" id="myModalLabel">Exploração Agrícola</h3>
+						</div>
+						<div class="panel-body">
+							<form id="registerForm" method="POST" action="/exploracoes/adicionar/submit" >
+								<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">		
+								<div class="form-group">
+									<fieldset> 
+										<legend>Dados do Terreno</legend>
+										<div class="col-xs-12 col-md-12">
+											<label for="nome">Nome do Terreno</label>
+											<div class="input-group">
+												<input type="text" class="form-control" id="nome"  name="nome" placeholder="Insira o nome do terreno" required><span class="input-group-addon"><i class="glyphicon glyphicon-asterisk"></i></span>
+											</span>										
+										</div>
+										@if( Session::get('message'))
+										<div style="text-align: center">
+											<span class="alert alert-info"> {{ Session::get('message') }}</span>
+										</div>
+										@endif
+										<br>
+										<label for="numero">Número do Terreno</label>
+										<div>
+											<input type="number" class="form-control" id="numero"  name="numero" min=0 placeholder="Insira o número de registo do terreno">
+											</div>
+									</fieldset>
 								</div>
+								<div class="form-group">
+									<fieldset> 
+										<legend>Localização</legend>
+										<div class="col-xs-12 col-md-6">
 
-							</fieldset>
-						</div>
-						<div class="form-group">
-							<fieldset> 
-								<legend>Localização</legend>
-								<div class="col-xs-12 col-md-6">
+											<label for="distrito">Distrito</label>
+											<div>
+												<input type="text" class="form-control" id="distrito" name="distrito" placeholder="Insira o distrito onde se localiza o terreno" >
+											</div>
+											<br>
+											<label for="concelho">Concelho</label>
+											<div>
+												<input type="text" class="form-control" id="concelho" name="concelho" placeholder="Insira o concelho onde se localiza o terreno" >
+											</div><br>
+										</div> 
 
-									<label for="distrito">Distrito</label>
-									<div>
-										<input type="text" class="form-control" id="distrito" name="distrito" placeholder="Insira o distrito onde se localiza o terreno" >
+										<div class="col-xs-12 col-md-6"> 
+											<label for="freguesia">Freguesia</label>
+											<div>
+												<input type="text" class="form-control" id="freguesia"  name="freguesia" placeholder="Insira a freguesia onde se localiza o terreno">
+
+											</div>
+											<br>
+											<label for="area">Área</label>
+											<div>
+												<input type="text" class="form-control" id="area" name="area" placeholder="Insira a área total do terreno">
+											</div><br>
+										</div>  
+									</fieldset>
+								</div>    
+								<div class="form-group">
+									<div class="input-group-addon">
+										<a href="/exploracoes/listar" role="button" name="cancelar"class="btn btn-default pull-right">Cancelar</a>
+										<input type="submit" name="submit" id="submit" value="Gravar" class="btn btn-success pull-right">
 									</div>
-									<br>
-									<label for="concelho">Concelho</label>
-									<div>
-										<input type="text" class="form-control" id="concelho" name="concelho" placeholder="Insira o concelho onde se localiza o terreno" >
-									</div><br>
-								</div> 
-
-								<div class="col-xs-12 col-md-6"> 
-									<label for="freguesia">Freguesia</label>
-									<div>
-										<input type="text" class="form-control" id="freguesia"  name="freguesia" placeholder="Insira a freguesia onde se localiza o terreno">
-
-									</div>
-									<br>
-									<label for="area">Área</label>
-									<div>
-										<input type="text" class="form-control" id="area" name="area" placeholder="Insira a área total do terreno">
-									</div><br>
-								</div>  
-							</fieldset>
-						</div>    
-						<div class="form-group">
-							<div class="input-group-addon">
-								<a href="/exploracoes/listar" role="button" name="cancelar"class="btn btn-default pull-right">Cancelar</a>
-								<input type="submit" name="submit" id="submit" value="Gravar" class="btn btn-success pull-right">
-							</div>
+								</div>
+							</form>
 						</div>
-					</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-</div>
-@endsection
+</body>
+<script src="{{asset('js/jquery-2.1.4.js')}}"></script>
+<script src="{{asset('js/angular.js')}}"></script>
+<script src="{{asset('js/bootstrap.js')}}"></script>
+</html>
 
-
-@section('customScripts')
-
-@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,46 +19,44 @@
   <div class="row">
    <div class="Absolute-Center is-Responsive">
     <div id="logo-container"></div>
-     <div class="panel panel-default">
-        <div class="panel-heading">
-          <h1 class="panel-title" id="panelTitle">Great Greenhouse</h1>
-        </div>
-        <div class="panel-body">
-    <div class="col-sm-12 col-md-10 col-md-offset-1">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1 class="panel-title" id="panelTitle">Great Greenhouse</h1>
+      </div>
+      <div class="panel-body">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
 
-      <form class="form-signin" method="POST" action="/register/registration">
-       <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-         <div id="registerText">
-        <span class="form-signin-heading" >Insira os seus dados</span>
-        <div class="form-group input-group">
-          <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span></span>
-          <input class="form-control" type="text" name='name' placeholder="name"/>          
+          <form class="form-signin" method="POST" action="/register/registration">
+           <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+           <div id="registerText">
+            <span class="form-signin-heading" >Insira os seus dados</span>
+            <div class="form-group input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+              <input class="form-control" type="text" name='name' placeholder="name"/>          
+            </div>
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+            <input class="form-control" type="text" name='email' placeholder="email"/>          
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input class="form-control" type="password" name='password' placeholder="password"/>     
+          </div>
 
-        </div>
-
-        </div>
-        <div class="form-group input-group">
-          <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span></span>
-          <input class="form-control" type="text" name='email' placeholder="email"/>          
-        </div>
-        <div class="form-group input-group">
-          <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-          <input class="form-control" type="password" name='password' placeholder="password"/>     
-        </div>
-
-        <div class="form-group input-group">
-          <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-          <input class="form-control" type="password" name='password_confirmation' placeholder="password confirmation"/>     
-        </div>
-        <div class="form-group">
-          <button type="button" class="btn btn-default pull-right">Cancel</button>        
-          <button type="submit" class="btn btn-default pull-right">Register</button>
-        </div>  
-        </div>
-      </form>        
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input class="form-control" type="password" name='password_confirmation' placeholder="password confirmation"/>     
+          </div>
+          <div class="form-group">
+            <button type="button" class="btn btn-default pull-right">Cancel</button>        
+            <button type="submit" class="btn btn-default pull-right">Register</button>
+          </div>  
+        </form>    
+      </div>
     </div>  
-  </div>    
-</div>
+  </div>   
+</div>   
 </div>
 </body>
 <script src="js/jquery-2.1.4.js"></script>
