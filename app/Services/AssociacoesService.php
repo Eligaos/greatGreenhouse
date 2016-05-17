d16ac3261113d1e6d92b0d3a8f386b1a4e88a2fd
Jounter <ruben.n147@gmail.com>
2016-05-17 18:47:59 +0100

    asd

--- app/Services/AssociacoesService.php
+++ app/Services/AssociacoesService.php
@@ -15,7 +15,7 @@ class AssociacoesService
 	public function listarAssociacoes($estufas){ //da exp atual
 		$tudo = [];
 		for($i=0; $i<count($estufas);$i++){		
-			$join = Estufa::join('setores', 'estufas.id', '=', 'setores.estufa_id')->join('associacoes','setores.id', '=','associacoes.setor_id')->join('tipo_leitura', 'associacoes.tipo_id', '=', 'tipo_leitura.id')->select('estufas.id as estufa_id', 'associacoes.id as associacoes_id','tipo_leitura.id as tipo_leitura_id', 'estufas.nome as estufa_nome', 'tipo_leitura.parametro', 'tipo_leitura.unidade')->where('estufas.id', '=', $estufas[$i]->id)->get();
+			$join = Estufa::join('setores', 'estufas.id', '=', 'setores.estufa_id')->join('associacoes','setores.id', '=','associacoes.setor_id')->join('sensores', 'associacoes.sensor_id', '=', 'sensores.id')->join('tipo_leitura','sensores.')->select('estufas.id as estufa_id', 'associacoes.id as associacoes_id','sensores.id as sensores_id', 'estufas.nome as estufa_nome', 'tipo_leitura.parametro', 'tipo_leitura.unidade')->where('estufas.id', '=', $estufas[$i]->id)->get();
 			array_push($tudo,$join);
 		}
 		$associacoes = [];
