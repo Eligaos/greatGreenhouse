fef24afb7ad155a4d904c12eeb8aad371f4f049f
Eligaos <david_barbosa13@hotmail.com>
2016-05-29 19:38:04 +0100

    grafico labels

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -4,37 +4,32 @@ $(function() {
    $.get("/admin/leituras/ultimas/"+estufas.data[j].id, function(dados) {
 
    }).done(function(dados){
-    var data = [];
+    /*var data = [];
+
     for (var i =0; i < dados.length; i++) {
       data.push({
         time: dados[i].data,
-        value: dados[i].valor
+        value: dados[i].valor 
       });
 
-    }
-
+    }*/
+    console.log(dados);
     new Morris.Line({
       // ID of the element in which to draw the chart.
       element: 'monitor'+dados[0].estufa_id,
       // Chart data records -- each entry in this array corresponds to a point on
       // the chart.
-
-      data: data,
-      /*
-      data: [
-        { year: '2008', value: 20 },
-        { year: '2009', value: 10 },
-        { year: '2010', value: 5 },
-        { year: '2011', value: 5 },
-        { year: '2012', value: 20 }
-        ],*/
+      resize: true,
+      data: dados,
       // The name of the data record attribute that contains x-values.
-      xkey: 'time',
+      xkey: 'data',
       // A list of names of data record attributes that contain y-values.
-      ykeys: ['value'],
+      ykeys: ['valor'],
       // Labels for the ykeys -- will be displayed when you hover over the
       // chart.
-      labels: ['Valor']
+      postUnits: dados[0].unidade,
+      labels: [dados[0].parametro]
+
     });
     
   });
