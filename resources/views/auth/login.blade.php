a9ee623da52d2f093ff26103c35e1f90ad016680
Eligaos <david_barbosa13@hotmail.com>
2016-07-03 20:54:14 +0100

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,57 +15,51 @@
     <link href="{{asset('css/home/login.css')}}" rel="stylesheet">
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <div class="Absolute-Center is-Responsive">
-            <div id="logo-container"></div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h1 class="panel-title" id="panelTitle">Great Greenhouse</h1>
-                </div>
-                <div class="panel-body">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-md-offset-2 col-xs-offset-2 col-sm-offset-2">
-                        <div id="loginText">
-                            <span>Entre na sua conta</span> <span>Ou</span>
-                            <a href="/register">
-                                <button class="btn btn-xs btn-primary " type="submit">Registe-se</button>
-                            </a>
-                            <hr>
-                        </div>
-                        <form method="POST" id="loginForm" action="/authentication/auth">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                            <div class="form-group input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span></span>
-                                <input class="form-control" type="text" name='nome' placeholder="nome de utilizador"/>
-                            </div>
-                            <div class="form-group input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-
-                                <input class="form-control" type="password" name='palavra-passe' placeholder="palavra-passe"/>
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> <span>Lembra-me</span>
-                                </label>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-success btn-block">Entrar</button>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="Absolute-Center is-Responsive">
+                <div id="logo-container"></div>
+                <div class="panel panel-default">
+                    <div class="panel-heading margin-bottom">
+                        <h1 class="panel-title" id="panelTitle">Great Greenhouse</h1>
+                    </div>
+                    <div class="panel-body">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-md-offset-2 col-xs-offset-2 col-sm-offset-2">
+                            <div id="loginText">
+                                <span>Entre na sua conta</span> <span>Ou</span>
+                                <a href="/register">
+                                    <button class="btn btn-xs btn-primary " type="submit">Registe-se</button>
+                                </a>
+                                <hr>
                             </div>
                             @if (Session::has('errors'))
-                                <br>
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors as $error)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors as $error)
 
-                                            <li>{{ $error}}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
+                                    <li>{{ $error}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                             @endif
-                            <div class="form-group text-center">
-                                <hr>
-                                <a href="#">Esqueceu-se da Password?</a>
+                            <form method="POST" id="loginForm" action="/authentication/auth">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                <div class="form-group input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span></span>
+                                    <input class="form-control" type="text" name='nome' placeholder="nome de utilizador"/>
+                                </div>
+                                <div class="form-group input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+
+                                    <input class="form-control" type="password" name='palavra-passe' placeholder="palavra-passe"/>
+                                </div>
+                                <div class="text-center margin-bottom">
+                                    <button type="submit" class="btn btn-success btn-block">Entrar</button>
+                                </div>
+                            </div>
+                            <div class="panel-footer">
+
                             </div>
                         </form>
                     </div>
